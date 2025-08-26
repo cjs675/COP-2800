84aa69e43a5bf63b49e1816d07f04e96b190d956
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-08-26 17:39:15 -0400

    add java class that prints Hello World to std.o

---
+++ assignment-1/HelloWorld.java
@@ -0,0 +1,18 @@
+// single line comments 
+
+
+/* multi-line comments
+
+the purpose of this program is to 
+
+write a program which takes input from std (i)o stream, 
+
+and output it to the standard output (window) 
+
+*/
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
