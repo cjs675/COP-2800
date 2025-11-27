bac29936e1393976ca96fe894ee375ad7a6361df
cjs675 <148303992+cjs675@users.noreply.github.com>
2025-11-26 20:51:16 -0500

    add formHandler function to extract user input from form

--- java-web-app/SimpleWebApplication/src/main/java/com/example/simplewebapplication/HomePageController.java
+++ java-web-app/SimpleWebApplication/src/main/java/com/example/simplewebapplication/HomePageController.java
@@ -3,6 +3,8 @@ package com.example.simplewebapplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /*
     Returns HTML from /templates
@@ -14,6 +16,14 @@ public class HomePageController
     public String home(Model model)
     {
         model.addAttribute("name", "Spring Boot");
+        return "index";
+    }
+
+    @PostMapping("/submit")
+    public String formHandler(@RequestParam("userInput") String userInput, Model model)
+    {
+        model.addAttribute("name", "Spring Boot");
+        model.addAttribute("submittedValue", userInput);
         return "index";
     }
 }
